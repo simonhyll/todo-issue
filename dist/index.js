bcf381150949b2dc76e06a4eb95c3bce870f148b
Simon Hyll <hyllsimon@gmail.com>
2022-12-09 07:27:52 +0100

    New release

--- dist/index.js
+++ dist/index.js
@@ -706,7 +706,7 @@ function generateTodosFromCommit() {
     return __awaiter(this, void 0, void 0, function* () {
         const todos = [];
         // RegEx that matches lines with the configured keywords
-        const regex = new RegExp(`^(?<beforeTag>\\W+)(?<keyword>${ArgumentContext_1.argumentContext.keywords.join('|')}|todo\\!\\(\\")\\b(?<title>((?!.)|(?!\\").)+)`, (!ArgumentContext_1.argumentContext.caseSensitive ? 'i' : ''));
+        const regex = new RegExp(`^(?<beforeTag>\\W+)(?<keyword>${ArgumentContext_1.argumentContext.keywords.join('|')}|todo\\!\\(\\")\\b\\S*(?<title>((?!.)|(?!\\").)+)`, (!ArgumentContext_1.argumentContext.caseSensitive ? 'i' : ''));
         let files;
         // Diff as files or import all
         if (ArgumentContext_1.argumentContext.importAll) {
